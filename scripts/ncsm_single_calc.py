733ccd0f65f360e9f3c4a7dc789a8046084f1f70
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-03-18 12:06:09 -0700

    Added verbose option to single calc

--- scripts/ncsm_single_calc.py
+++ scripts/ncsm_single_calc.py
@@ -31,34 +31,49 @@ from scripts.ncsm_vce_calc import ncsd_exact_calculations
 if __name__ == "__main__":
     user_args = argv[1:]
     force0, exact_range = False, False
+    verbose = False
     while True:
         a0 = user_args[0]
         if '-f' == a0.lower():
             force0 = True
         elif '-e' == a0.lower():
             exact_range = True
+        elif '-v' == a0.lower():
+            verbose = True
         else:
             break
         user_args = user_args[1:]
     if not exact_range:
         if len(user_args) == 2:
             z0, a0 = [int(x) for x in user_args]
-            ncsd_single_calculation(z=z0, a=a0, aeff=a0, force=force0)
+            ncsd_single_calculation(
+                z=z0, a=a0, aeff=a0,
+                force=force0, verbose=verbose
+            )
         elif len(user_args) == 3:
             z0, a0, aeff0 = [int(x) for x in user_args]
-            ncsd_single_calculation(z=z0, a=a0, aeff=aeff0, force=force0)
+            ncsd_single_calculation(
+                z=z0, a=a0, aeff=aeff0,
+                force=force0, verbose=verbose
+            )
         elif len(user_args) == 4:
             z0, a0, aeff0, nhw0 = [int(x) for x in user_args]
-            ncsd_single_calculation(z=z0, a=a0, aeff=aeff0,
-                                    nhw=nhw0, force=force0)
+            ncsd_single_calculation(
+                z=z0, a=a0, aeff=aeff0, nhw=nhw0,
+                force=force0, verbose=verbose
+            )
         elif len(user_args) == 5:
             z0, a0, aeff0, n1_0, n2_0 = [int(x) for x in user_args]
-            ncsd_single_calculation(z=z0, a=a0, aeff=aeff0,
-                                    n1=n1_0, n2=n2_0, force=force0)
+            ncsd_single_calculation(
+                z=z0, a=a0, aeff=aeff0, n1=n1_0, n2=n2_0,
+                force=force0, verbose=verbose
+            )
         elif len(user_args) == 6:
             z0, a0, aeff0, nhw0, n1_0, n2_0 = [int(x) for x in user_args]
-            ncsd_single_calculation(z=z0, a=a0, aeff=aeff0,
-                                    nhw=nhw0, n1=n1_0, n2=n2_0, force=force0)
+            ncsd_single_calculation(
+                z=z0, a=a0, aeff=aeff0, nhw=nhw0, n1=n1_0, n2=n2_0,
+                force=force0, verbose=verbose
+            )
         else:
             raise InvalidNumberOfArgumentsException(
                 '%d' % len(user_args) +
@@ -68,23 +83,34 @@ if __name__ == "__main__":
     else:
         if len(user_args) == 2:
             z0, amin = [int(x) for x in user_args]
-            ncsd_exact_calculations(z=z0, a_range=[amin], force=force0)
+            ncsd_exact_calculations(
+                z=z0, a_range=[amin],
+                force=force0, verbose=verbose
+            )
         elif len(user_args) == 3:
             z0, amin, amax = [int(x) for x in user_args]
-            ncsd_exact_calculations(z=z0, a_range=range(amin, amax+1),
-                                    force=force0)
+            ncsd_exact_calculations(
+                z=z0, a_range=range(amin, amax+1),
+                force=force0, verbose=verbose
+            )
         elif len(user_args) == 4:
             z0, amin, amax, nhw0 = [int(x) for x in user_args]
-            ncsd_exact_calculations(z=z0, a_range=range(amin, amax+1),
-                                    nhw=nhw0, force=force0)
+            ncsd_exact_calculations(
+                z=z0, a_range=range(amin, amax+1), nhw=nhw0,
+                force=force0, verbose=verbose
+            )
         elif len(user_args) == 5:
             z0, amin, amax, n1_, n2_ = [int(x) for x in user_args]
-            ncsd_exact_calculations(z=z0, a_range=range(amin, amax+1),
-                                    n1=n1_, n2=n2_, force=force0)
+            ncsd_exact_calculations(
+                z=z0, a_range=range(amin, amax+1), n1=n1_, n2=n2_,
+                force=force0, verbose=verbose
+            )
         elif len(user_args) == 6:
             z0, amin, amax, nhw0, n1_, n2_ = [int(x) for x in user_args]
-            ncsd_exact_calculations(z=z0, a_range=range(amin, amax+1),
-                                    nhw=nhw0, n1=n1_, n2=n2_, force=force0)
+            ncsd_exact_calculations(
+                z=z0, a_range=range(amin, amax+1), nhw=nhw0, n1=n1_, n2=n2_,
+                force=force0, verbose=verbose
+            )
         else:
             raise InvalidNumberOfArgumentsException(
                 '%d' % len(user_args) +
