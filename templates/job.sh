4ce9ff82f25367c431f98ea7ca809757ee0ccd62
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-03-29 09:11:22 -0700

    Fixed job submit file

--- templates/job.sh
+++ templates/job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #PBS -l walltime=<<WALLTIME>>
-#PBS -l nodes=1:ppn=${NTHREADS}
+#PBS -l nodes=1:ppn=12
 #PBS -l vmem=60gb
 #PBS -m ae
 #PBS -M dilyn.fullerton@alumni.ubc.ca
 #PBS -j oe 
 
 NTHREADS=12
-export OMP_NUM_THREADS=$NUM_THREADS
+export OMP_NUM_THREADS=$NTHREADS
 
 cd $PBS_O_WORKDIR
 
