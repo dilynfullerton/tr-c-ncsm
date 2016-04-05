fac6e0bd51d0cc8d0e541fc8e52ec0dc37bfd185
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-04-05 16:29:01 -0700

    Off-diagonal scale function

--- scripts/F_scale_interaction.py
+++ scripts/F_scale_interaction.py
@@ -14,19 +14,20 @@ _LINE_FMT = ' %2d' * 6 + ' %11.6f' * 6 + '\n'
 
 def ecf_off_diag_outside_valence(nshell):
     """Returns an elt_cond_fn that returns true if matrix element is off
-    diagonal and outside the valence range
+    diagonal and couples the valence + core space to the outside space
+    Assumes that the interaction file's format satisfies:
+        (1) a <= b
+        (2) a <= c
+        (3) c <= d
+    Due to this format, we want a, b in the core + valence space and c or d in
+    the outside space.
     :param nshell: major oscillator shell (0=s, 1=p, 2=sd, ...)
     """
-    idx_max = 1/2 + nshell
+    idx_max = (nshell + 1) * (nshell + 2) / 2
 
     def elt_cond_fn(elt):
         a, b, c, d, j, t, = elt
-        # todo is this correct?
-        if (a > idx_max or b > idx_max or c > idx_max or d > idx_max and
-                set([a, b]) != set([c, d])):
-            return True
-        else:
-            return False
+        return a <= idx_max and b <= idx_max < d
     return elt_cond_fn
 
 
