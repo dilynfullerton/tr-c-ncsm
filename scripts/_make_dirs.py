b124a92962ab678ce7be355046b57370b51275fa
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-04-06 17:40:17 -0700

    Removed debugging print statements

--- scripts/_make_dirs.py
+++ scripts/_make_dirs.py
@@ -255,21 +255,15 @@ def _truncate_space(
             break
     else:
         raise TbmeFileNotFoundException()
-    print 'Found template TBME file: %s' % tbme_filename
     src_path = path.join(dpath_templates, tbme_filename)
     tmp_path = path.join(dpath_elt, FNAME_FMT_TBME % n1)
     if scalefactor is None:
         dst_path = tmp_path
-        print 'No scale factor provided, destination path is: %s' % dst_path
     else:
         dst_path = path.join(dpath_elt, FNAME_FMT_TBME_SF % (n1, scalefactor))
-        print 'Scale factor is %f' % scalefactor
-        print 'Destination path is %s' % dst_path
     if not path.exists(dst_path):
-        print 'Destination path does not exist; truncating space...'
         truncate_interaction(src_path, n1, n2, tmp_path)
         if scalefactor is not None:
-            print 'Scaling interaction...'
             scale_int(src=tmp_path, dst=dst_path, nshell=nshell,
                       scalefactor=scalefactor)
             remove(tmp_path)
