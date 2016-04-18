584e3c501c4034935b05e81266362fafeeb88ed6
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-04-18 13:17:08 -0700

    Shell script get_results.sh: Ignore *.egv files

--- get_results.sh
+++ get_results.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-rsync -Hrl --exclude="*.tmp" cougar:~/NCSM/calc/mcalc/results ./
+from='cougar:~/NCSM/calc/mcalc/results';
+to='./';
+rsync -Hrl \
+  --exclude="*.tmp" \
+  --exclude="*.egv" \
+  $from $to;
