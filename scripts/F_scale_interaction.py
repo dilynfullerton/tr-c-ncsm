32a8377c1cbf745e55206e30a9208405411c8be2
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-04-18 14:55:49 -0700

    F_scale_interaction.py : Further abstracted get_scaleif_fn to allow the user to specify method of scaling (e.g. multiplication by scale factor, addition by scale factor, etc). Added a condition function for scaling only elements coupling the space outside the valence region to itself.

--- scripts/F_scale_interaction.py
+++ scripts/F_scale_interaction.py
@@ -32,6 +32,19 @@ def ecf_off_diag_outside_valence(nshell):
     return elt_cond_fn
 
 
+def ecf_outside_valence(nshell):
+    """Returns an element-condition-function that returns true if the matrix
+    element couples the space outside core + valence to itself
+    :param nshell: major oscillator shell (0=s, 1=p, 2=sd, ...)
+    """
+    idx_max_valn = (nshell + 1) * (nshell + 2) / 2
+
+    def elt_cond_fn(elt):
+        a, b, c, d, j, t = elt
+        return b > idx_max_valn and d > idx_max_valn
+    return elt_cond_fn
+
+
 def ecf_isospin(t):
     """Returns an elt_cond_fn that returns true (do scaling) if isospin is
     the given value
@@ -43,7 +56,8 @@ def ecf_isospin(t):
     return elt_cond_fn
 
 
-def get_scaleif_fn(elt_cond_fn, rest_cols_to_scale, scalefactor):
+def get_scaleif_fn(elt_cond_fn, rest_cols_to_scale, scalefactor,
+                   scale_method=lambda a, b: a * b):
     """Returns a scale function that does the scaling only if
     elt_cond_fn is satisfied
     :param elt_cond_fn: function from the tuple (a, b, c, d, j, t) to a
@@ -52,16 +66,33 @@ def get_scaleif_fn(elt_cond_fn, rest_cols_to_scale, scalefactor):
     to be scaled. These can be relative to the first float column [0, 6) or
     relative to the first column [6, 12)
     :param scalefactor: float value by which to scale rest_cols
+    :param scale_method: function that defines how each column is updated by
+    the scale factor; default is to multiply self by scale factor, but
+    alternatives could include dividing, adding, subtracting, etc
     :return: function that takes elt and rest as arguments and returns a
     tuple of scaled "rest" values, where
     elt is the tuple (a, b, c, d, j, t) and
-    rest is the tuple of associated float values,
+    rest is the tuple of associated float values
+    (Trel, Hrel, Coulomb, Vpn, Vpp, Vnn)
     """
     def scaleif(elt, rest):
         if elt_cond_fn(elt):
             for col in rest_cols_to_scale:
                 col_idx = col % 6
-                rest[col_idx] = scalefactor * rest[col_idx]
+                rest[col_idx] = scale_method(rest[col_idx], scalefactor)
+        return rest
+    return scaleif
+
+
+def get_combined_scaleif_fn(scaleif_functions):
+    """Given a list of scale-if functions of the form f(elt, rest) -> rest,
+    returns a scale-if function that composes these in reversed order
+    :param scaleif_functions: list of scale-if functions to be applied
+    in reversed order
+    """
+    def scaleif(elt, rest):
+        for fn in reversed(scaleif_functions):
+            rest = fn(elt, rest)
         return rest
     return scaleif
 
@@ -99,6 +130,12 @@ def run(fpath_src, fpath_dst, scalefn0=lambda a, b: b, force=False,
 def scale_off_diag_outside_valence(src, dst, nshell, scalefactor):
     scalefn = get_scaleif_fn(
         elt_cond_fn=ecf_off_diag_outside_valence(nshell),
-        rest_cols_to_scale=[0, 3, 4, 5], scalefactor=scalefactor
+        rest_cols_to_scale=[0, 3, 4, 5], scalefactor=scalefactor,
     )
+    # scalefn2 = get_scaleif_fn(
+    #     elt_cond_fn=ecf_outside_valence(nshell),
+    #     rest_cols_to_scale=[0, 3, 4, 5], scalefactor=20.0,
+    #     scale_method=lambda a, b: a + b,
+    # )
+    # scalefn = get_combined_scaleif_fn([scalefn, scalefn2])
     return run(fpath_src=src, fpath_dst=dst, scalefn0=scalefn)
