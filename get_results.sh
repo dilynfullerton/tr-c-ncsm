7dcdd25b2a8dd1de04315041f53c4b377a08f21a
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-04-18 17:11:29 -0700

    q

--- get_results.sh
+++ get_results.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+# cougar -> here
 from='cougar:~/NCSM/calc/mcalc/results';
 to='./';
+
+# here -> itheory
+# from='./results';
+# to='itheory:~/workspace/tr-c-ncsm/';
+
 rsync -Hrl \
   --exclude="*.tmp" \
   --exclude="*.egv" \
