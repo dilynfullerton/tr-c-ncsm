20d7451f753a6a53a1c6cc3ac001fa7fc519ca32
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-05-15 01:29:27 -0700

    Added file that demonstrates effect of mpirun

--- get_results.sh
+++ get_results.sh
@@ -2,14 +2,15 @@
 # pulls relevant results from cougar to the current directory
 
 # cougar -> here
-from='cougar:~/NCSM/calc/mcalc/results';
+#from='cougar:~/NCSM/calc/mcalc/results';
+from='cougar:~/NCSM/calc/mcalc/try';
 to='./';
 
 # here -> itheory
 # from='./results';
 # to='itheory:~/workspace/tr-c-ncsm/';
 
-rsync -Hrl \
+rsync -Hrlv \
   --exclude="*.tmp" \
   --exclude="*.egv" \
   $from $to;
