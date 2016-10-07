f5bb880bbaf83088a30723383c497c4749f49558
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-10-07 15:30:28 -0700

    Removed alpha=2 parameter - Needs verification

--- nuclear_codes/it-code-111815.f
+++ nuclear_codes/it-code-111815.f
@@ -2416,13 +2416,13 @@ c     DRF Determine term to add
                   if (laa.eq.lbb.and.jaa.eq.jbb) then
                      if (naa.eq.nbb) then
                         trelaeff = trelaeff +
-     &                       pp*(2*naa+laa+1.5)/8.0
+     &                       pp*(2*naa+laa+1.5)/16.0
                      elseif (naa.eq.nbb+1) then
                         trelaeff = trelaeff +
-     &                       pp*sqrt(naa*(naa+laa+0.5))/8.0
+     &                       pp*sqrt(naa*(naa+laa+0.5))/16.0
                      elseif (nbb.eq.naa+1) then
                         trelaeff = trelaeff +
-     &                       pp*sqrt(nbb*(nbb+lbb+0.5))/8.0
+     &                       pp*sqrt(nbb*(nbb+lbb+0.5))/16.0
                      endif
                   endif
                endif
