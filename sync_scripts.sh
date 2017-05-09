c4cde884197b1e63791d09a549c6bc0a3f096394
Dilyn Fullerton <dilyn.fullerton@att.net>
2017-05-09 14:12:20 -0500

    Changed lithium script to 456 presc

--- sync_scripts.sh
+++ sync_scripts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # puts python scripts in the scripts directory onto cougar
 from='scripts/*.py';
-to='cougar:~/NCSM/calc/mcalc/scripts/';
+to='cougar:/itch/dfullerton/NCSM/calc/mcalc/scripts/';
 rsync -r ${from} ${to};
