19d0e7f5d310ee070691d24a595cf0d17f095c8e
Dilyn Fullerton <dilyn.fullerton@att.net>
2017-05-10 16:01:47 -0500

    Remove redundant calculation from n=z

--- scripts/calc-p-nEQz.py
+++ scripts/calc-p-nEQz.py
@@ -2,14 +2,13 @@
 
 from ncsm_vce_calc import ncsd_vce_calculations, ncsd_exact_calculations
 
-AP_RANGE = range(4, 15+1)
 NMAX = 2
 N_SHELL = 1  # p shell
 
-for z0 in AP_RANGE:
+for z0 in range(2, 7):
     ncsd_vce_calculations(
-        a_prescriptions=[[z0, z0, z0]],
-        a_range=[z0],
+        a_prescriptions=[[2*z0, 2*z0, 2*z0]],
+        a_range=[2*z0],
         z=z0,
         nmax=NMAX,
         nshell=N_SHELL,
@@ -17,24 +16,10 @@ for z0 in AP_RANGE:
         n1=15, n2=15,
         int_scalefactor=None,
         cluster_submit=True,
-        walltime='48:00:00',
+        walltime='6:00:00',
         remove_protons=False,
         force_ncsd=False,
         force_trdens=True,
         force_all=False,
         progress=False,
-    )
-
-    ncsd_exact_calculations(
-        a_range=[z0],
-        z=z0,
-        nmax=NMAX,
-        nshell=N_SHELL,
-        n1=15, n2=15,
-        int_scalefactor=None,
-        cluster_submit=True,
-        walltime='48:00:00',
-        force=False,
-        progress=False,
-        remove_protons=False,
     )
